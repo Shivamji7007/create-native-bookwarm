bac01ca5245c72061d49ab1df76b7dada758f5c1
Shivam Pandey <shivampandeyji7007@gmail.com>
2025-05-12 20:48:45 +0530

    crone.js added

--- src/lib/crone.js
+++ src/lib/crone.js
@@ -1,13 +1,13 @@
 import cron from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * *", function() {
+const job = new cron.CronJob("0 */14 * * * *", function() {
   https 
   .get( process.env.API_URL, (res)=> {
     if(res.statusCode === 200) console.log ("GET request send successfully");
     else console.log("GET request failed", res.statusCode);
   })
-  .on("error", (e) => console.log("Error while  sending request", e));
+  .on("error", (e) => console.error("Error while  sending request", e));
 
 });
 export default job;
